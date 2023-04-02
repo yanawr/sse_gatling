6f5d834b2a904803f99e6c40bf28c98348bd8331
yanawr <yana.v.wagner@gmail.com>
2023-04-03 00:38:24 +0200

    Error line 27
    cannot find symbol
      symbol:   variable setCheck
      location: class io.gatling.javaapi.http.Sse
    sse("SetCheck").setCheck

--- SseSandboxGatlingJ.java
+++ SseSandboxGatlingJ.java
@@ -6,6 +6,8 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 import io.gatling.javaapi.http.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
+import io.gatling.http.action.sse.*;
+
 
 /**
  * This sample is based on our official tutorials:
@@ -19,17 +21,21 @@ public class SseSandboxGatlingJ extends Simulation {
     HttpProtocolBuilder httpProtocol =
         http.baseUrl("https://wil5k.sse.codesandbox.io");
 
-    ChainBuilder sseConnect =
-            exec(sse("01_connect").connect("/es")
+    SseMessageCheck sseCheckPushmessages = sse.checkMessage("checkPushmessages")
+                            .check(regex("message\",\"id\":\"(.+?)\""));
+
+    SseSetCheckBuilder acc = sse("SetCheck").setCheck;
+
+    ScenarioBuilder users = scenario("Users")//.exec(sseConnect, sseClose);
+        .exec(sse("01_connect").connect("/es")
                 .await(10).on(
-                        sse.checkMessage("checkCustom").check(substring("custom")))
-            );
-    
-    ChainBuilder sseClose =
-            exec(sse("02_Close").close());
-
-
-    ScenarioBuilder users = scenario("Users").exec(sseConnect, sseClose);
+                    sse.checkMessage("checkCustom").check(substring("custom")))
+        )
+        .repeat(200).on(
+            exec(acc.await(60).on(sseCheckPushmessages()))
+        )
+        .pause(10)
+        .exec(sse("02_Close").close());
 
     {
         setUp(users.injectOpen(rampUsers(1).during(10))
